276ad80e1d1daa271745fea4bc66757e76c794d9
Adriano Avelino <adriano-st777@hotmail.com>
2021-04-07 21:25:29 -0300

    adiciona endpoint

---
+++ src/main/java/br/con/adrianoavelino/springdockerdevenvironment/conrtroller/OlaMundoController.java
@@ -0,0 +1,19 @@
+package br.con.adrianoavelino.springdockerdevenvironment.conrtroller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.con.adrianoavelino.springdockerdevenvironment.model.OlaMundo;
+
+@RestController
+@RequestMapping("/olamundo")
+public class OlaMundoController {
+
+	@GetMapping
+	public ResponseEntity<OlaMundo> exibirMensagem() {
+		return new ResponseEntity<>(new OlaMundo("Olá, Mundo!!"), HttpStatus.OK);
+	}
+}
---
+++ src/main/java/br/con/adrianoavelino/springdockerdevenvironment/model/OlaMundo.java
@@ -0,0 +1,18 @@
+package br.con.adrianoavelino.springdockerdevenvironment.model;
+
+public class OlaMundo {
+	
+	private String mensagem;
+
+	public OlaMundo() {
+	}
+
+	public OlaMundo(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+}
