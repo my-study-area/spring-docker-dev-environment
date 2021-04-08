8eaef61e0ac8719d7e0ccbdc6ba1ac52f505017f
Adriano Avelino <adriano-st777@hotmail.com>
2021-04-07 21:24:55 -0300

    inicia controle de versão

---
+++ src/main/java/br/con/adrianoavelino/springdockerdevenvironment/SpringDockerDevEnvironmentApplication.java
@@ -0,0 +1,13 @@
+package br.con.adrianoavelino.springdockerdevenvironment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerDevEnvironmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerDevEnvironmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/con/adrianoavelino/springdockerdevenvironment/SpringDockerDevEnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.con.adrianoavelino.springdockerdevenvironment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerDevEnvironmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
